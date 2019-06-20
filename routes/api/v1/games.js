343fb6cdf25606d5ac30607ab50d617a0e90aaf5
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-06-20 13:19:15 -0600

    Adds show route for single game

--- routes/api/v1/games.js
+++ routes/api/v1/games.js
@@ -14,4 +14,20 @@ router.get("/", function(req, res, next) {
     });
 });
 
+router.get("/:id", function(req, res, next) {
+  Game.findAll( {
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(games => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).send(JSON.stringify(games));
+    })
+    .catch(error => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(500).send({error})
+    });
+});
+
 module.exports = router;
